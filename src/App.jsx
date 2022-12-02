5eed619bac9a7b406d7ea61daefa238c3f448700
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-02 18:53:24 -0500

    inicios caja escritora

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { width } from "@mui/system";
 import React from "react";
+import { Container } from "@mui/material";
+import Searcher from "./componets/Searcher"
 
 const App = () => {
     return(
-        <div>Soy Saori</div>
+        <Container sx= {{
+            background:"whitesmoke",
+            width:"80vw",
+            height: "500px",
+            borderRadius: "16px",
+            marginTop:"40px",
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center",
+        }} >
+        <Searcher>Soy Saori</Searcher>
+        </Container>
     )
 };
 
---
+++ src/componets/Searcher/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IconButton, Stack, TextField } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+const Searcher = () => {
+    return (
+    <Stack>
+        <TextField>
+            
+        </TextField>
+        <IconButton></IconButton>
+    </Stack>
+)
+}
+
+export default Searcher;
