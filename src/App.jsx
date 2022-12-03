8013c1ac0382b668c123e8c6e0cbc20e9107d617
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-03 16:54:54 -0500

    if not found

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,30 @@ import { getGitHubUser } from "./services/users"
 const App = () => {
 
     const [inputUser, setInputUser] = useState('saorionline');
-    const [userStarter, userState] = useState('inputUser');
+    const [userState, setUserState] = useState('inputUser');
+    const [ notFound, setNotFound ] = useState(false);
 
     const gettinUser = async (user) => {
         const userResponse = await getGitHubUser(user)
-        console.log(userResponse);
-    }
+        
+        if(userState == 'saorionline'){
+            localStorage.setItem('saorionline', userResponse)
+        }
+
+        if (userResponse.message == 'Not Found'){
+            const { saorionline } = localStorage;
+            setInputUser(saorionline);
+            setNotFound(true);
+        } else {
+            setUserState(userResponse);
+        }
+    }; 
+    
+    console.log(userState);
+    
     useEffect(() =>{
         gettinUser(inputUser)
-    },[])
+    },[inputUser])
 
     return(
         <Container sx= {{
