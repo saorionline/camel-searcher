07378bd5fa4e33fd50c2851ff963e068a87ce637
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-04 11:37:52 -0500

    more icons to describe info

--- src/containers/Description/index.jsx
+++ src/containers/Description/index.jsx
@@ -1,7 +1,7 @@
  import React, {Fragment} from "react";
  import { Stack, Typography } from "@mui/material";
  import PaperInfo from "../../componets/PaperInfo";
- import LocationInfo from "react"
+ import LocationInfo from "../../componets/LocationInfo"
 
  const Description = (props) => {
     const { userState } = props;
@@ -9,13 +9,13 @@
     return(
         <Fragment>
             <Stack>
-                {bio != null
+                {bio !== null
                 ?<Typography>{bio}</Typography>
                 :<Typography>About the profile</Typography>
             }
             </Stack>
             <PaperInfo userState={userState}/>
-            <LocationInfo/>
+            <LocationInfo userState={userState}/>
         </Fragment>
     )
  };
