2a8a066e54d6842a395422f754f5a174e3796af5
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-04 12:04:23 -0500

    giving style to headings and caption

--- src/containers/UserCard/index.jsx
+++ src/containers/UserCard/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CardMedia, Grid } from "@mui/material";
 import MainInfo from "../../componets/MainInfo";
 import Description from "../Description";
+import { Stack } from "@mui/system";
 
 const UserCard = (props) => {
 const { userState } = props
@@ -9,18 +10,31 @@ const { avatar_url } = userState
 
 
 return(
-    <Grid container spacing={2}>
+    <Grid 
+    container 
+    spacing={2}
+    sx={{ marginTop:'15px' }}>
         <Grid item xs={3}>
             <CardMedia
                 component="img"
                 alt = "GitHub User"
                 image={avatar_url}
+                sx={{
+                    borderRadius:'50%',
+                    marginLeft: '5px'
+                }}
             />
         </Grid>
         <Grid item xs={9}>
+            <Stack
+                direction="column"
+                spacing={1}
+                sx={{ margin: '23px'}}>
             <MainInfo userState={userState}/>
-        </Grid>
             <Description userState={userState}/>
+            </Stack>
+        </Grid>
+            
     </Grid>
 )
 }
